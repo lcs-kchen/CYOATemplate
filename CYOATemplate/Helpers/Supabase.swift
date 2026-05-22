f58f9cd6a04a8e50e212d00c203cee3c783d3599
Veda Cunniffe <vcunniffe@lcs.on.ca>
2026-05-21 22:52:22 -0400

    Replaced the supabase URL with the one sttached to our project. Have not replaced the api key because am unsure if am choosing the right key to copy-paste.

--- CYOATemplate/Helpers/Supabase.swift
+++ CYOATemplate/Helpers/Supabase.swift
@@ -9,7 +9,7 @@ import Foundation
 import Supabase
 
 let supabase = SupabaseClient(
-  supabaseURL: URL(string: "https://mrdjbpjpvgtqxznsgtee.supabase.co")!,
+  supabaseURL: URL(string: "https://wwwqkzsnksxhycpxsfrt.supabase.co")!,
   supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yZGpicGpwdmd0cXh6bnNndGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTczMzIyMzgsImV4cCI6MjAzMjkwODIzOH0.kLM3U980_U3qjFvw-5kvUERqSsIiB_-HTHa2FlhM5wU"
 )
 
