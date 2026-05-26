27d03010bfe12dd54a80942dc637db05d3eee2e9
Kevin Chen <kchen@lcs.on.ca>
2026-05-26 14:51:21 -0400

    Created player model

---
+++ Shared/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  CYOATemplate
+//
+//  Created by Haowen Chen on 2026-05-26.
+//
+
+import Foundation
+
+struct Player: Identifiable, Codable, Hashable {
+    let id: UUID
+    let email: String
+}
