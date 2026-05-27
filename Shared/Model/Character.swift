e483f16a222d20f6740cb528dbfc31b07fa856fb
Veda Cunniffe <vcunniffe@lcs.on.ca>
2026-05-27 13:43:14 -0400

    Created a character model.

---
+++ Shared/Model/Character.swift
@@ -0,0 +1,39 @@
+//
+//  Character.swift
+//  CYOATemplate
+//
+//  Created by Veda Niav Cunniffe on 2026-05-27.
+//
+
+import Foundation
+
+// MODEL
+//
+// NOTE: Model names should be singular       
+struct Character: Identifiable {
+    
+    // MARK: Stored properties
+    // Stored properties must be provided with a value by providing an argument when creating an instance of this structure, or, be initialized with a default value
+    
+    // Unique identifier to conform to Identifiable protocol
+    // This is initialized with a default value
+    let id = UUID()
+    
+
+    
+    // MARK: Computed properties
+    // Computed properties calculate or derive a value using stored properties
+    
+    
+    
+    // MARK: Functions
+    // Functions take action using information provided through parameters
+    
+    
+
+}
+
+// Create several instances of your structure...
+
+
+// Now create an array containing the instances above
